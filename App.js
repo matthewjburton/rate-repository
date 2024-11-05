e91796e61ac862a8330e9eb1b0e1d6172acf1cf4
matthewjburton <mattjburton64@gmail.com>
2024-11-05 13:32:01 -0500

    10.12

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import Main from './src/components/Main';
 import { NativeRouter as Router } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
 import createApolloClient from './src/utils/apolloClient';
+import Constants from 'expo-constants';
 
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.expoConfig);
   return (
     <Router>
       <ApolloProvider client={apolloClient}>
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,20 +1,20 @@
 import { useQuery } from '@apollo/client';
 import { useState, useEffect } from 'react';
 import { GET_REPOSITORIES } from '../graphql/queries';
+import Constants from 'expo-constants';
 
 const useRepositories = () => {
-  /*
   const [repositories, setRepositories] = useState();
   const [loading, setLoading] = useState(false);
 
   const fetchRepositories = async () => {
     setLoading(true);
 
-    const response = await fetch('http://192.168.0.100:5001/api/repositories');
+    const response = await fetch(Constants.expoConfig.extra.apiUri);
     const json = await response.json();
 
     setLoading(false);
-    setRepositories(data);
+    setRepositories(json);
   };
 
   useEffect(() => {
@@ -22,15 +22,16 @@ const useRepositories = () => {
   }, []);
 
   return { repositories, loading, refetch: fetchRepositories };
-  */
 
+  /*
   const { data, error, loading } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
   });
 
-  const repositories = loading ? undefined : data.repositories;
+  const repositories = loading ? undefined : data?.repositories;
 
   return { repositories, loading };
+  */
 };
 
 export default useRepositories;
